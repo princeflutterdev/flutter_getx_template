661bc5ab4c475c0d4e473cd2576fe4505eb6db30
Prince Negi <princeflutterdev@gmail.com>
2024-09-15 03:29:18 +0530

    first commit

---
+++ lib/app/modules/home/bindings/home_binding.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeBinding extends Binding {
+  @override
+  List<Bind> dependencies() {
+    return [
+      Bind.lazyPut(() => HomeController()),
+    ];
+  }
+}
